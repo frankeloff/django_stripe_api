a296152366f86480f86e641fb102f8ffec1b814e
frankeloff <ruskiborg@list.ru>
2023-02-19 19:44:08 +0300

    replaced the request with a function call

--- my_api/stripe_api/context_processors.py
+++ my_api/stripe_api/context_processors.py
@@ -1,5 +1,4 @@
-from .models import Order
-
+from .stripe_api_services import get_products_in_basket_by_session
 
 def getting_basket_info(request):
     session_key = request.session.session_key
@@ -7,7 +6,7 @@ def getting_basket_info(request):
         request.session["session_key"] = 123
         request.session.cycle_key()
 
-    products_in_basket = Order.objects.filter(session_key=session_key)
+    products_in_basket = get_products_in_basket_by_session(session_key=session_key)
     products_total_nmb = products_in_basket.count()
 
     return {
